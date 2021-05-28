8b63b00256f00dd92fce87062f80a229e9309f40
nikugogoi <95nikass@gmail.com>
2021-05-28 14:56:14 +0530

    Add test for unsigned integer type.

--- packages/solidity-mapper/test/storage.test.ts
+++ packages/solidity-mapper/test/storage.test.ts
@@ -9,9 +9,20 @@ describe("Storage", function() {
     const integers = await Integers.deploy();
     await integers.deployed();
 
+    let value = 12;
+    await integers.setInt1(value);
+    let storageValue = await getStorageValue(hre, "TestIntegers", "int1", integers.address);
+    expect(storageValue).to.equal(value);
+  });
+
+  it("get value for unsigned integer type", async function() {
+    const UnsignedIntegers = await hre.ethers.getContractFactory("TestUnsignedIntegers");
+    const unsignedIntegers = await UnsignedIntegers.deploy();
+    await unsignedIntegers.deployed();
+
     const value = 123;
-    await integers.setInt3(value);
-    const storageValue = await getStorageValue(hre, "TestIntegers", "int3", integers.address)
-    expect(storageValue).to.equal(value)
+    await unsignedIntegers.setUint1(value);
+    const storageValue = await getStorageValue(hre, "TestUnsignedIntegers", "uint1", unsignedIntegers.address);
+    expect(storageValue).to.equal(value);
   });
 });
