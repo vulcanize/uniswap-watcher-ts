2010f8cee301247a9f1c8021e1434ca1b8f891d2
nikugogoi <95nikass@gmail.com>
2021-05-31 12:04:22 +0530

    Get slot for ERC20 variable from storage layout.

--- packages/watcher/src/resolvers.ts
+++ packages/watcher/src/resolvers.ts
@@ -4,6 +4,9 @@ import debug from 'debug';
 
 import { getCache } from '@vulcanize/cache';
 import { EthClient, getMappingSlot, topictoAddress } from '@vulcanize/ipld-eth-client';
+import { getStorageInfo } from '@vulcanize/solidity-mapper';
+
+import { storageLayout } from './artifacts/ERC20.json';
 
 // Event slots.
 // TODO: Read from storage layout file.
@@ -55,7 +58,8 @@ export const createResolvers = async (config) => {
       balanceOf: async (_, { blockHash, token, owner }) => {
         log('balanceOf', blockHash, token, owner);
 
-        const slot = getMappingSlot(ERC20_BALANCE_OF_SLOT, owner);
+        const { slot: balancesSlot } = getStorageInfo(storageLayout, '_balances')
+        const slot = getMappingSlot(balancesSlot, owner);
 
         const vars = {
           blockHash,
@@ -89,6 +93,7 @@ export const createResolvers = async (config) => {
       allowance: async (_, { blockHash, token, owner, spender }) => {
         log('allowance', blockHash, token, owner, spender);
 
+        const { slot: allowancesSlot } = getStorageInfo(storageLayout, '_allowances')
         const slot = getMappingSlot(getMappingSlot(ERC20_ALLOWANCE_SLOT, owner), spender);
 
         const vars = {
