7e6a6f2e0fe318606b135f018bf4d013a664dd37
nikugogoi <95nikass@gmail.com>
2021-07-01 17:09:05 +0530

    Setup resolvers with uniswap info schema.

--- packages/uni-info-watcher/src/server.ts
+++ packages/uni-info-watcher/src/server.ts
@@ -1,16 +1,23 @@
 import assert from 'assert';
 import 'reflect-metadata';
 import express, { Application } from 'express';
-import { ApolloServer } from 'apollo-server-express';
+import { ApolloServer, PubSub } from 'apollo-server-express';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import debug from 'debug';
 import 'graphql-import-node';
 import { createServer } from 'http';
 
+import { getCache } from '@vulcanize/cache';
+import { EthClient } from '@vulcanize/ipld-eth-client';
+
 import typeDefs from './schema';
 
 import { createResolvers as createMockResolvers } from './mock/resolvers';
+import { createResolvers } from './resolvers';
+import { Indexer } from './indexer';
+import { Database } from './database';
+import { EventWatcher } from './events';
 import { getConfig } from './config';
 
 const log = debug('vulcanize:server');
@@ -31,8 +38,31 @@ export const main = async (): Promise<any> => {
 
   const { host, port } = config.server;
 
-  // const resolvers = process.env.MOCK ? await createMockResolvers() : await createResolvers(indexer);
-  const resolvers = await createMockResolvers();
+  const { upstream, database: dbConfig } = config;
+
+  assert(dbConfig, 'Missing database config');
+
+  const db = new Database(dbConfig);
+  await db.init();
+
+  assert(upstream, 'Missing upstream config');
+  const { gqlEndpoint, gqlSubscriptionEndpoint, cache: cacheConfig } = upstream;
+  assert(gqlEndpoint, 'Missing upstream gqlEndpoint');
+  assert(gqlSubscriptionEndpoint, 'Missing upstream gqlSubscriptionEndpoint');
+
+  const cache = await getCache(cacheConfig);
+
+  const ethClient = new EthClient({ gqlEndpoint, gqlSubscriptionEndpoint, cache });
+
+  // Note: In-memory pubsub works fine for now, as each watcher is a single process anyway.
+  // Later: https://www.apollographql.com/docs/apollo-server/data/subscriptions/#production-pubsub-libraries
+  const pubsub = new PubSub();
+  const indexer = new Indexer(db, ethClient, pubsub);
+
+  const eventWatcher = new EventWatcher(ethClient, indexer);
+  await eventWatcher.start();
+
+  const resolvers = process.env.MOCK ? await createMockResolvers() : await createResolvers(indexer);
 
   const app: Application = express();
   const server = new ApolloServer({
