50a08515fce67733db0ddab482b52125f635b5d4
nikugogoi <95nikass@gmail.com>
2021-07-01 20:19:47 +0530

    Add mock query for Burn entity.

--- packages/uni-info-watcher/src/mock/data.ts
+++ packages/uni-info-watcher/src/mock/data.ts
@@ -1,4 +1,5 @@
 import Chance from 'chance';
+import { ethers } from 'ethers';
 
 export interface Entity {
   blockNumber: number
@@ -10,10 +11,14 @@ export class Data {
   static _instance: Data;
 
   _entities: {[key: string]: Array<Entity>} = {
-    bundles: []
+    bundles: [],
+    burns: [],
+    transactions: [],
+    pools: [],
+    tokens: []
   }
 
-  _chance
+  _chance: Chance.Chance
 
   constructor () {
     this._chance = new Chance();
@@ -35,11 +40,97 @@ export class Data {
     // Generate data for 3 blocks.
     Array.from(Array(3))
       .forEach((_, index) => {
+        // Generate Bundle.
         this._entities.bundles.push({
           blockNumber: index,
           id: '1',
           ethPriceUSD: this._chance.floating({ min: 0, fixed: 2 })
         });
+
+        // Generate Pools.
+        Array.from(Array(3))
+          .forEach(() => {
+            const token0 = {
+              blockNumber: index,
+              id: this._getRandomAddress(),
+              symbol: this._chance.string({ length: 3, casing: 'upper', alpha: false }),
+              name: this._chance.word({ syllables: 1 }),
+              volume: this._chance.integer({ min: 1 }),
+              volumeUSD: this._chance.floating({ min: 1, fixed: 2 }),
+              feesUSD: this._chance.floating({ min: 1, fixed: 2 }),
+              txCount: this._chance.integer({ min: 1 }),
+              totalValueLocked: this._chance.integer({ min: 1 }),
+              totalValueLockedUSD: this._chance.floating({ min: 1, fixed: 2 }),
+              derivedETH: this._chance.floating({ min: 1, fixed: 2 })
+            };
+
+            const token1 = {
+              blockNumber: index,
+              id: this._getRandomAddress(),
+              symbol: this._chance.string({ length: 3, casing: 'upper', alpha: false }),
+              name: this._chance.word({ syllables: 1 }),
+              volume: this._chance.integer({ min: 1 }),
+              volumeUSD: this._chance.floating({ min: 1, fixed: 2 }),
+              feesUSD: this._chance.floating({ min: 1, fixed: 2 }),
+              txCount: this._chance.integer({ min: 1 }),
+              totalValueLocked: this._chance.integer({ min: 1 }),
+              totalValueLockedUSD: this._chance.floating({ min: 1, fixed: 2 }),
+              derivedETH: this._chance.floating({ min: 1, fixed: 2 })
+            };
+
+            const pool = {
+              blockNumber: index,
+              id: this._getRandomAddress(),
+              token0: token0.id,
+              token1: token1.id,
+              feeTier: this._chance.floating({ min: 1, fixed: 2 }),
+              liquidity: this._chance.integer({ min: 1 }),
+              sqrtPrice: this._chance.floating({ min: 1, fixed: 2 }),
+              token0Price: this._chance.floating({ min: 1, fixed: 2 }),
+              token1Price: this._chance.floating({ min: 1, fixed: 2 }),
+              tick: this._chance.integer({ min: 1 }),
+              volumeUSD: this._chance.floating({ min: 1, fixed: 2 }),
+              txCount: this._chance.integer({ min: 1 }),
+              totalValueLockedToken0: this._chance.integer({ min: 1 }),
+              totalValueLockedToken1: this._chance.integer({ min: 1 }),
+              totalValueLockedUSD: this._chance.floating({ min: 1, fixed: 2 })
+            };
+
+            this._entities.tokens.push(token0, token1);
+            this._entities.pools.push(pool);
+
+            // Generate Transactions.
+            Array.from(Array(3))
+              .forEach((_, transactionIndex) => {
+                const transactionHash = ethers.utils.hexlify(ethers.utils.randomBytes(32));
+
+                const transaction = {
+                  blockNumber: index,
+                  id: transactionHash,
+                  timestamp: this._chance.timestamp()
+                };
+
+                this._entities.transactions.push(transaction);
+
+                // Generate Burns
+                this._entities.burns.push({
+                  id: `${transaction.id}#${transactionIndex}`,
+                  blockNumber: index,
+                  transaction: transaction.id,
+                  pool: pool.id,
+                  timestamp: this._chance.timestamp(),
+                  owner: this._getRandomAddress(),
+                  origin: this._getRandomAddress(),
+                  amount0: this._chance.integer({ min: 1 }),
+                  amount1: this._chance.integer({ min: 1 }),
+                  amountUSD: this._chance.floating({ min: 1, fixed: 2 })
+                });
+              });
+          });
       });
+  }
+
+  _getRandomAddress (): string {
+    return ethers.utils.hexlify(ethers.utils.randomBytes(20));
   }
 }
--- packages/uni-info-watcher/src/mock/resolvers.ts
+++ packages/uni-info-watcher/src/mock/resolvers.ts
@@ -2,6 +2,8 @@ import debug from 'debug';
 import BigInt from 'apollo-type-bigint';
 import { Data, Entity } from './data';
 
+const LATEST_BLOCK = 2;
+
 const log = debug('test');
 
 interface BlockHeight {
@@ -9,9 +11,24 @@ interface BlockHeight {
   hash: string;
 }
 
+enum OrderDirection {
+  asc,
+  desc
+}
+
+enum BurnOrderBy {
+  timestamp
+}
+
+interface BurnFilter {
+  pool: string;
+  token0: string;
+  token1: string;
+}
+
 export const createResolvers = async (): Promise<any> => {
   const data = Data.getInstance();
-  const { bundles } = data.entities;
+  const { bundles, burns } = data.entities;
 
   return {
     BigInt: new BigInt('bigInt'),
@@ -29,9 +46,28 @@ export const createResolvers = async (): Promise<any> => {
 
       bundles: (_: any, { first, block }: { first: number, block: BlockHeight }) => {
         log('bundles', first, block);
+
         const res = bundles.filter((bundle: Entity) => bundle.blockNumber === block.number)
           .slice(0, first)
           .map(({ ethPriceUSD, id }) => ({ ethPriceUSD, id }));
+
+        return res;
+      },
+
+      burns: (_: any, { first, orderBy, orderDirection, where }: { first: number, orderBy: BurnOrderBy, orderDirection: OrderDirection, where: BurnFilter }) => {
+        log('burns', first, orderBy, orderDirection, where);
+
+        const res = burns.filter((burn: Entity) => {
+          if (burn.blockNumber === LATEST_BLOCK) {
+            return Object.entries(where || {})
+              .every(([field, value]) => burn[field] === value);
+          }
+
+          return false;
+        }).slice(0, first)
+          .sort((a: any, b: any) => {
+            return orderDirection === OrderDirection.asc ? (a - b) : (b - a);
+          });
 
         return res;
       }
