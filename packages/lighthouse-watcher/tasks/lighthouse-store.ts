bbf4b55cf4da7a4e3447d756bd9e77d5ddcffd5f
nabarun <nabarun@deepstacksoft.com>
2021-08-10 15:55:02 +0530

    Implement watcher for Lighthouse StorageRequest event.

---
+++ packages/lighthouse-watcher/tasks/lighthouse-store.ts
@@ -0,0 +1,38 @@
+import { task, types } from 'hardhat/config';
+import '@nomiclabs/hardhat-ethers';
+import { ContractTransaction } from 'ethers';
+
+task('lighthouse-store', 'Call Lighthouse store method')
+  .addParam('lighthouse', 'Address of Lighthouse contract', undefined, types.string)
+  .addParam('cid', 'store cid', undefined, types.string)
+  .addParam('storeConfig', 'store config', undefined, types.string)
+  .setAction(async (args, hre) => {
+    const {
+      lighthouse: lighthouseAddress,
+      cid,
+      storeConfig: config
+    } = args;
+
+    await hre.run('compile');
+
+    const Ligthouse = await hre.ethers.getContractFactory('Lighthouse');
+    const lighthouse = Ligthouse.attach(lighthouseAddress);
+
+    const transaction: ContractTransaction = await lighthouse.store(cid, config);
+
+    const receipt = await transaction.wait();
+
+    if (receipt.events) {
+      console.log('receipt blockHash', receipt.blockHash);
+
+      const storageRequestEvent = receipt.events.find(el => el.event === 'StorageRequest');
+
+      if (storageRequestEvent && storageRequestEvent.args) {
+        console.log('StorageRequest Event');
+        console.log('uploader:', storageRequestEvent.args.uploader);
+        console.log('cid:', storageRequestEvent.args.cid);
+        console.log('config:', storageRequestEvent.args.config);
+        console.log('fileCost:', storageRequestEvent.args.fileCost.toString());
+      }
+    }
+  });
