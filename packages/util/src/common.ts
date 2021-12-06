cerc-io/watcher-ts

Use postgraphile client to fetch blocks

packages/util/src/common.ts
@@ -50,12 +50,12 @@ export const processBlockByNumber = async (
   log(`Process block ${blockNumber}`);
 
   while (true) {
-    const result = await ethClient.getBlocksByNumber(blockNumber);
+    const result = await ethClient.getBlocks({ blockNumber });
     const { allEthHeaderCids: { nodes: blockNodes } } = result;
 
     if (blockNodes.length) {
       for (let bi = 0; bi < blockNodes.length; bi++) {
-        const { blockHash, blockNumber, parentHash, timestamp } = blockNodes[bi];
+        const { cid, blockHash, blockNumber, parentHash, timestamp } = blockNodes[bi];
         const blockProgress = await indexer.getBlockProgress(blockHash);
 
         if (blockProgress) {
@@ -63,7 +63,7 @@ export const processBlockByNumber = async (
         } else {
           await indexer.updateSyncStatusChainHead(blockHash, blockNumber);
 
-          await jobQueue.pushJob(QUEUE_BLOCK_PROCESSING, { kind: JOB_KIND_INDEX, blockHash, blockNumber, parentHash, timestamp });
+          await jobQueue.pushJob(QUEUE_BLOCK_PROCESSING, { kind: JOB_KIND_INDEX, cid, blockHash, blockNumber, parentHash, timestamp });
         }
       }
 

vulcanize/uniswap-watcher-ts

packages/util/src/common.ts
@@ -51,7 +51,7 @@ export const processBlockByNumber = async (
 
     if (blocks.length) {
       for (let bi = 0; bi < blocks.length; bi++) {
-        const { blockHash, blockNumber, parentHash, timestamp } = blocks[bi];
+        const { cid, blockHash, blockNumber, parentHash, timestamp } = blocks[bi];
         const blockProgress = await indexer.getBlockProgress(blockHash);
 
         if (blockProgress) {
@@ -59,7 +59,7 @@ export const processBlockByNumber = async (
         } else {
           await indexer.updateSyncStatusChainHead(blockHash, blockNumber);
 
-          await jobQueue.pushJob(QUEUE_BLOCK_PROCESSING, { kind: JOB_KIND_INDEX, blockHash, blockNumber, parentHash, timestamp });
+          await jobQueue.pushJob(QUEUE_BLOCK_PROCESSING, { kind: JOB_KIND_INDEX, cid, blockHash, blockNumber, parentHash, timestamp });
         }
       }
 