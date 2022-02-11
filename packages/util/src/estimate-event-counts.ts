dbaac915794190d74680302c823d567b32a87b68
prathamesh0 <prathamesh.musale0@gmail.com>
2022-02-11 14:37:37 +0530

    Use uppercase for constants

--- packages/util/src/estimate-event-counts.ts
+++ packages/util/src/estimate-event-counts.ts
@@ -10,33 +10,33 @@ import { wait } from './misc';
 const CACHE_DIR = 'out/cached-data';
 const RESULTS_DIR = 'out/estimation-results';
 
-const factoryAddress = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
-const nfpmAddress = '0xC36442b4a4522E871399CD717aBDD847Ab11FE88';
+const FACTORY_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
+const NFPM_ADDRESS = '0xC36442b4a4522E871399CD717aBDD847Ab11FE88';
 
-const factoryEventSignatureMap = {
+const FACTORY_EVENT_SIGNATURE_MAP = {
   PoolCreated: '0x783cca1c0412dd0d695e784568c96da2e9c22ff989357a2e8b1d9b2b4e6b7118'
 };
 
-const nfpmEventSignatureMap = {
+const NFPM_EVENT_SIGNATURE_MAP = {
   IncreaseLiquidity: '0x3067048beee31b25b2f1681f88dac838c8bba36af25bfb2b7cf7473a5847e35f',
   DecreaseLiquidity: '0x26f6a048ee9138f2c0ce266f322cb99228e8d619ae2bff30c67f8dcf9d2377b4',
   Collect: '0x40d0efd1a53d60ecbf40971b9daf7dc90178c3aadc7aab1765632738fa8b8f01',
   Transfer: '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
 };
 
-const poolEventSignatureMap = {
+const POOL_EVENT_SIGNATURE_MAP = {
   Initialize: '0x98636036cb66a9c19a37435efc1e90142190214e8abeb821bdba3f2990dd4c95',
   Mint: '0x7a53080ba414158be7ec69b987b5fb7d07dee101fe85488f0853ae16239d0bde',
   Burn: '0x0c396cd989a39f4459b5fa1aed6a9a8dcdbc45908acfd67e028cd568da98982c',
   Swap: '0xc42079f94a6350d7e6235f29174924f928cc2ac818eb64fed8004e115fbcca67'
 };
 
-const events = ['PoolCreated', 'IncreaseLiquidity', 'DecreaseLiquidity', 'Collect', 'Transfer', 'Initialize', 'Mint', 'Burn', 'Swap'];
+const EVENTS = ['PoolCreated', 'IncreaseLiquidity', 'DecreaseLiquidity', 'Collect', 'Transfer', 'Initialize', 'Mint', 'Burn', 'Swap'];
 
 // Etherscan API limitation
-const resultSizeLimit = 1000;
+const RESULT_SIZE_LIMIT = 1000;
 
-// Run: yarn estimate-event-counts --api-key <API-key>
+// Run: yarn estimate-event-counts --start-block [start-block] --end-block [end-block] --sample-size [sample-size] --interval [interval] --api-key <API-key>
 async function main () {
   const argv = await yargs.parserConfiguration({
     'parse-numbers': false
@@ -142,11 +142,11 @@ async function fetchSampleCounts (fromBlock: number, toBlock: number, apiKey: st
   let data: any[] = [];
 
   // Fetch factory events
-  const factoryResponsePromise = fetch(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=${toBlock}&address=${factoryAddress}&topic0=${factoryEventSignatureMap.PoolCreated}&apikey=${apiKey}`);
+  const factoryResponsePromise = fetch(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=${toBlock}&address=${FACTORY_ADDRESS}&topic0=${FACTORY_EVENT_SIGNATURE_MAP.PoolCreated}&apikey=${apiKey}`);
 
   // Fetch NFPM events
-  const nfpmResponsePromises = Object.values(nfpmEventSignatureMap).map(signature => {
-    return fetch(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=${toBlock}&address=${nfpmAddress}&topic0=${signature}&apikey=${apiKey}`);
+  const nfpmResponsePromises = Object.values(NFPM_EVENT_SIGNATURE_MAP).map(signature => {
+    return fetch(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=${toBlock}&address=${NFPM_ADDRESS}&topic0=${signature}&apikey=${apiKey}`);
   });
 
   let responses = await Promise.all([...[factoryResponsePromise], ...nfpmResponsePromises]);
@@ -161,7 +161,7 @@ async function fetchSampleCounts (fromBlock: number, toBlock: number, apiKey: st
   await wait(1000);
 
   // Fetch pool events
-  const poolResponsePromises = Object.values(poolEventSignatureMap).map(signature => {
+  const poolResponsePromises = Object.values(POOL_EVENT_SIGNATURE_MAP).map(signature => {
     return fetch(`https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=${fromBlock}&toBlock=${toBlock}&topic0=${signature}&apikey=${apiKey}`);
   });
 
@@ -182,8 +182,8 @@ async function fetchSampleCounts (fromBlock: number, toBlock: number, apiKey: st
   data.forEach((obj: any) => {
     assert(Array.isArray(obj.result), `Result "${obj.result}" is not an array`);
 
-    if (obj.result.length === resultSizeLimit) {
-      console.log(`WARNING: Result size reached limit (${resultSizeLimit}). Estimates might not be correct. Please reduce sample size.`);
+    if (obj.result.length === RESULT_SIZE_LIMIT) {
+      console.log(`WARNING: Result size reached limit (${RESULT_SIZE_LIMIT}). Estimates might not be correct. Please reduce sample size.`);
     }
 
     sampleEventCounts.push(obj.result.length);
@@ -231,7 +231,7 @@ async function exportResult (csvPath: string, counts: number[], totalEvents: num
 
   const records = counts.map((count, index) => {
     return {
-      event: events[index],
+      event: EVENTS[index],
       count
     };
   });
