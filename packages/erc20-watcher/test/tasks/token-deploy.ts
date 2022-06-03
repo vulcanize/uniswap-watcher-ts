bf731bd49db229e103bb8c6821194511e2636937
avivash <av@andrewvivash.com>
2022-06-03 16:16:00 -0700

    test: tweak token-deploy file

--- packages/erc20-watcher/test/tasks/token-deploy.ts
+++ packages/erc20-watcher/test/tasks/token-deploy.ts
@@ -2,18 +2,31 @@
 // Copyright 2021 Vulcanize, Inc.
 //
 
-import { task, types } from 'hardhat/config';
+// import { task, types } from 'hardhat/config';
+// import '@nomiclabs/hardhat-ethers';
+
+// const DEFAULT_INITIAL_SUPPLY = '1000000000000000000000';
+
+// task('token-deploy', 'Deploys GLD token')
+//   .addOptionalParam('initialSupply', 'Set total supply', DEFAULT_INITIAL_SUPPLY, types.string)
+//   .setAction(async (args, hre) => {
+//     const { initialSupply } = args;
+//     await hre.run('compile');
+//     const Token = await hre.ethers.getContractFactory('GLDToken');
+//     const token = await Token.deploy(hre.ethers.BigNumber.from(initialSupply));
+
+//     console.log('GLD Token deployed to:', token.address);
+//   });
+
+import { task } from 'hardhat/config';
 import '@nomiclabs/hardhat-ethers';
 
-const DEFAULT_INITIAL_SUPPLY = '1000000000000000000000';
-
-task('token-deploy', 'Deploys GLD token')
-  .addOptionalParam('initialSupply', 'Set total supply', DEFAULT_INITIAL_SUPPLY, types.string)
-  .setAction(async (args, hre) => {
-    const { initialSupply } = args;
+task('contract-deploy', 'Deploys PhisherRegistry contract').setAction(
+  async (args, hre) => {
     await hre.run('compile');
-    const Token = await hre.ethers.getContractFactory('GLDToken');
-    const token = await Token.deploy(hre.ethers.BigNumber.from(initialSupply));
+    const Contract = await hre.ethers.getContractFactory('PhisherRegistry');
+    const contract = await Contract.deploy();
 
-    console.log('GLD Token deployed to:', token.address);
-  });
+    console.log('PhisherRegistry deployed to:', contract.address);
+  }
+);
