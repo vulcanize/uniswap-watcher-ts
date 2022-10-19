0d3ecec784761d99737bc1c14101fa17d7da1ffd
nabarun <nabarun@deepstacksoft.com>
2022-10-19 10:42:11 +0530

    Clear pruned cached entities at intervals

--- packages/uni-info-watcher/src/indexer.ts
+++ packages/uni-info-watcher/src/indexer.ts
@@ -26,7 +26,8 @@ import {
   eventProcessingEthCallDuration,
   getFullTransaction,
   getFullBlock,
-  StateKind
+  StateKind,
+  cachePrunedEntitiesCount
 } from '@cerc-io/util';
 import { EthClient } from '@cerc-io/ipld-eth-client';
 import { StorageLayout, MappingKey } from '@cerc-io/solidity-mapper';
@@ -254,8 +255,17 @@ export class Indexer implements IndexerInterface {
           entities: new Map()
         }
       );
+    }
 
-      log(`Size of cachedEntities.frothyBlocks map: ${this._db.cachedEntities.frothyBlocks.size}`);
+    log(`Size of cachedEntities.frothyBlocks map: ${this._db.cachedEntities.frothyBlocks.size}`);
+    this._measureCachedPrunedEntities();
+
+    // Check if it is time to clear entities cache.
+    if (blockProgress.blockNumber % this._serverConfig.clearEntitiesCacheInterval === 0) {
+      log(`Clearing cachedEntities.latestPrunedEntities at block ${blockProgress.blockNumber}`);
+      // Clearing only pruned region as frothy region cache gets updated in pruning queue.
+      this._db.cachedEntities.latestPrunedEntities.clear();
+      log(`Cleared cachedEntities.latestPrunedEntities. Map size: ${this._db.cachedEntities.latestPrunedEntities.size}`);
     }
   }
 
@@ -656,6 +666,14 @@ export class Indexer implements IndexerInterface {
   _clearCachedEntities () {
     this._db.cachedEntities.frothyBlocks.clear();
     this._db.cachedEntities.latestPrunedEntities.clear();
+  }
+
+  _measureCachedPrunedEntities () {
+    const totalEntities = Array.from(this._db.cachedEntities.latestPrunedEntities.values())
+      .reduce((acc, idEntitiesMap) => acc + idEntitiesMap.size, 0);
+
+    log(`Total entities in cachedEntities.latestPrunedEntities map: ${totalEntities}`);
+    cachePrunedEntitiesCount.set(totalEntities);
   }
 
   async _fetchEvents (block: DeepPartial<BlockProgress>): Promise<DeepPartial<Event>[]> {
