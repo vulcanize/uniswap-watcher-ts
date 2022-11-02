5710c5f8ba658b2b57ce12d70d392ba3777db82e
prathamesh0 <prathamesh.musale0@gmail.com>
2022-11-02 10:50:40 +0530

    Use import-state CLI from cli package

--- packages/uni-info-watcher/src/events.ts
+++ packages/uni-info-watcher/src/events.ts
@@ -7,7 +7,7 @@ import debug from 'debug';
 import { PubSub } from 'apollo-server-express';
 
 import { EthClient } from '@vulcanize/ipld-eth-client';
-import { QUEUE_BLOCK_PROCESSING, QUEUE_EVENT_PROCESSING } from '@cerc-io/util';
+import { QUEUE_BLOCK_PROCESSING, QUEUE_EVENT_PROCESSING, IndexerInterface } from '@cerc-io/util';
 import { EventWatcher as BaseEventWatcher, EventWatcherInterface, JobQueue, UpstreamConfig } from '@vulcanize/util';
 
 import { Indexer } from './indexer';
@@ -141,9 +141,9 @@ export class EventWatcher implements EventWatcherInterface {
   _jobQueue: JobQueue
   _baseEventWatcher: BaseEventWatcher
 
-  constructor (upstreamConfig: UpstreamConfig, ethClient: EthClient, indexer: Indexer, pubsub: PubSub, jobQueue: JobQueue) {
+  constructor (upstreamConfig: UpstreamConfig, ethClient: EthClient, indexer: IndexerInterface, pubsub: PubSub, jobQueue: JobQueue) {
     this._ethClient = ethClient;
-    this._indexer = indexer;
+    this._indexer = indexer as Indexer;
     this._pubsub = pubsub;
     this._jobQueue = jobQueue;
     this._baseEventWatcher = new BaseEventWatcher(upstreamConfig, this._ethClient, this._indexer, this._pubsub, this._jobQueue);
