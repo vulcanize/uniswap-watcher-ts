ddc22792621a7f09672a112797798b816742456a
nabarun <nabarun@deepstacksoft.com>
2022-11-04 18:12:27 +0530

    Add latest entity tables

---
+++ packages/uni-info-watcher/src/entity/LatestToken.ts
@@ -0,0 +1,22 @@
+//
+// Copyright 2021 Vulcanize, Inc.
+//
+
+import { Entity, PrimaryColumn, Column, Index } from 'typeorm';
+import { graphDecimalTransformer, GraphDecimal, bigintTransformer } from '@vulcanize/util';
+
+@Entity()
+@Index(['totalValueLockedUSD'])
+export class LatestToken {
+  @PrimaryColumn('varchar', { length: 42 })
+  id!: string;
+
+  @PrimaryColumn('varchar', { length: 66 })
+  latestBlockHash!: string
+
+  @Column('integer')
+  latestBlockNumber!: number;
+
+  @Column('numeric', { default: 0, transformer: graphDecimalTransformer })
+  totalValueLockedUSD!: GraphDecimal;
+}
