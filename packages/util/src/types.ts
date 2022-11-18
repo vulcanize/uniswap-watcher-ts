83f65d16098c77e2222c50fb8e68d099b007bea8
prathamesh0 <prathamesh.musale0@gmail.com>
2022-11-18 16:01:35 +0530

    Remove unused method from indexer interface

--- packages/util/src/types.ts
+++ packages/util/src/types.ts
@@ -2,11 +2,8 @@
 // Copyright 2021 Vulcanize, Inc.
 //
 
-import { DeepPartial } from 'typeorm';
-
-import { IndexerInterface as BaseIndexerInterface, BlockProgressInterface, EventInterface } from '@cerc-io/util';
+import { IndexerInterface as BaseIndexerInterface, EventInterface } from '@cerc-io/util';
 
 export interface IndexerInterface extends BaseIndexerInterface {
-  saveBlockProgress (block: DeepPartial<BlockProgressInterface>): Promise<BlockProgressInterface>
   saveEvents (dbEvents: EventInterface[]): Promise<void>
 }
